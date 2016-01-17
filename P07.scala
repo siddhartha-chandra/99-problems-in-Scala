ea71eb8466b22f417edaa09a93ce7d00385a7183
siddhartha-chandra <siddhartha-chandra@github.com>
2016-01-17 03:17:57 -0500

    added suggested solution

--- P07.scala
+++ P07.scala
@@ -1,4 +1,4 @@
-/*P07- Flatten a nested List*/
+/*P07- Flatten a nested list structure*/
 object P07{
   def appendListList1To2[A](lst1: List[A], lst2: List[A]): List[A] = lst1 match {
     case Nil => lst2
@@ -8,5 +8,10 @@ object P07{
   def flattenList[A](lst: List[List[A]], res: List[A] = Nil):List[A] = lst match {
     case Nil => res
     case h::t => flattenList(t, appendListList1To2(h, res))
+  }
+
+  def flattenAnswer(ls: List[Any]): List[Any] = ls flatMap {
+    case ms: List[_] => flattenAnswer(ms)
+    case e => List(e)
   }
 }
