092d8506b52443c86993ca4349459e21609f4558
siddhartha-chandra <siddhartha.chandra0112@gmail.com>
2016-03-20 22:48:17 -0400

    modified to use P10.encode instead of P09.pack

--- P11.scala
+++ P11.scala
@@ -3,14 +3,14 @@ eg.
 scala> encodeModified(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
 res0: List[Any] = List((4,'a), 'b, (2,'c), (2,'a), 'd, (4,'e))
 */
-import P09.pack
+import P10.encodeUsingPack
 
 object P11{
   def encodeModified[A](ls: List[A]): List[Any] = {
-    pack(ls).map(x=> if (x.length == 1) x.head else (x.length, x.head))
+    encodeUsingPack(ls).map(x=> if (x._1 == 1) x._1 else x)
   }
 
   def encodeModifiedTypesafe[A](ls: List[A]): List[Either[A, (Int, A)]] = {
-    pack(ls).map(x=> if (x.length == 1) Left(x.head) else Right(x.length, x.head))
+    encodeUsingPack(ls).map(x=> if (x._1 == 1) Left(x._2) else Right(x))
   }
 }
