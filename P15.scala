c938c424857b2c272d52e84239f0586ed2dcf3c3
siddhartha-chandra <siddhartha.chandra0112@gmail.com>
2016-04-04 02:37:05 -0400

    P15 - Duplicate the elements of a list a given number of times.

---
+++ P15.scala
@@ -0,0 +1,8 @@
+/** P15 - Duplicate the elements of a list a given number of times.
+eg.
+scala> duplicateN(3, List('a, 'b, 'c, 'c, 'd))
+res0: List[Symbol] = List('a, 'a, 'a, 'b, 'b, 'b, 'c, 'c, 'c, 'c, 'c, 'c, 'd, 'd, 'd)
+*/
+object P15{
+  def duplicateN[A](n: Int, ls: List[A]): List[A] = ls flatMap {e => List.tabulate(n)(_ => e) }
+}
