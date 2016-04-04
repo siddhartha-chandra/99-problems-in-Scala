b48be79ffd6caf21083197184af9964ca9c3b7c4
siddhartha-chandra <siddhartha.chandra0112@gmail.com>
2016-04-04 02:29:31 -0400

    P14 - Duplicate the elements of a list.

---
+++ P14.scala
@@ -0,0 +1,17 @@
+/** P14 - Duplicate the elements of a list.
+eg.
+scala> duplicate(List('a, 'b, 'c, 'c, 'd))
+res0: List[Symbol] = List('a, 'a, 'b, 'b, 'c, 'c, 'c, 'c, 'd, 'd)
+*/
+
+object P14{
+  def duplicate[A](ls: List[A]): List[A] = {
+    def duplicateInternal(ls: List[A], acc: List[A] = Nil):List[A] = ls match {
+      case Nil => acc
+      case h::t => duplicateInternal(t, acc ++ List.tabulate(2)(_ => h))
+    }
+    duplicateInternal(ls)
+  }
+
+  def duplicateSolution[A](ls: List[A]): List[A] = ls flatMap { e => List(e, e) }
+}
