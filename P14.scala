bf920d41a8943d52b713cec11e39c9289ccf0012
siddhartha-chandra <siddhartha.chandra0112@gmail.com>
2016-04-16 17:36:09 -0400

    code cleanup

--- P14.scala
+++ P14.scala
@@ -3,7 +3,6 @@ eg.
 scala> duplicate(List('a, 'b, 'c, 'c, 'd))
 res0: List[Symbol] = List('a, 'a, 'b, 'b, 'c, 'c, 'c, 'c, 'd, 'd)
 */
-
 object P14{
   def duplicate[A](ls: List[A]): List[A] = {
     def duplicateInternal(ls: List[A], acc: List[A] = Nil):List[A] = ls match {
@@ -12,6 +11,5 @@ object P14{
     }
     duplicateInternal(ls)
   }
-
   def duplicateSolution[A](ls: List[A]): List[A] = ls flatMap { e => List(e, e) }
 }
